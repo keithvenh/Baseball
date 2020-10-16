4725d690a391b5ed0e954fd8c78599d62a2cee45
Keith VenHuizen <keith.venhuizen@gmail.com>
2020-10-16 10:11:24 -1000

    Add devise user models and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception, prepend: true
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def hello
         render html: "hello, world!"
     end
-    
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) {
+                |u| u.permit(:first_name, :last_name, :username, :email, :password)
+            }
+            devise_parameter_sanitizer.permit(:account_update) {
+                |u| u.permit(:first_name, :last_name, :username, :email, :password, :current_password)
+            }
+        end
 end
